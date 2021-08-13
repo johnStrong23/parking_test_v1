498449ce8d4d73da199c0909ad7913d75e8056c6
johnStrong23 <88659692+johnStrong23@users.noreply.github.com>
2021-08-13 11:01:05 +0300

    Update parking_part2.py

--- parking_part2.py
+++ parking_part2.py
@@ -1,6 +1,19 @@
+# **Install Detectron2**
 
-# **Run a Pre-Trained Detectron2 Model**
 ---
+
+
+
+#Need to install an old version of pytorch
+
+!pip install pyyaml==5.1
+
+# workaround: install old version of pytorch since detectron2 hasn't released packages for pyTorch 1.9
+!pip install torch==1.8.0+cu101 torchvision==0.9.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+
+# install detectron2 that matches pytorch 1.8 
+!pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.8/index.html
+# **Run a Pre-Trained Detectron2 Model**
 
 
 #Prepare the Mask-RCNN PreTrained Model (based on COCO Dataset)
